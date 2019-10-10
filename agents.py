f554fd0a37d176e5a5019aa8abde6f6d210c4df8
timtody <j.tay@hotmail.de>
2019-10-10 12:04:41 +0200

    Debug print shapes in train_iv_model

--- agents.py
+++ agents.py
@@ -97,6 +97,8 @@ class JointAgent:
         return metrics_dict
 
     def _train_iv_model(self, trans):
+        print(f"old:\
+              {trans["old"].shape}\nnew:{trans["new"].shape}\ntrans["actions"].shape)
         history = self.iv_model.fit(
             [trans["old"], trans["new"]], trans["actions"])
         metrics_dict = {"iv_model_loss": history.history["loss"][0]}
