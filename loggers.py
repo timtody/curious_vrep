37be2c9bcc6342d303ad9daef4b0ac8f1f4460e5
timtody <j.tay@hotmail.de>
2019-10-11 16:42:49 +0200

    Fix typo

--- loggers.py
+++ loggers.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 
 class Logger:
-    def __init(self, path):
-        self.tb_callback = self._create_tb_callback()
+    def __init__(self, path):
+        self.tb_callback = self._create_tb_callback(path)
 
     def _create_tb_callback(self, path):
         cb = tf.keras.callbacks.TensorBoard(log_dir=path)
